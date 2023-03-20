b220ddb7818024922447a3b29e1a6e781b54bf1a
aarontravass <aaron.travass@hotmail.com>
2023-03-20 20:06:54 +0000

    add more code

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,8 +1,15 @@
 import { Component, EventEmitter, OnInit, Output, ViewChild } from '@angular/core';
 import { MoveChange, NgxChessBoardView } from 'ngx-chess-board';
 import { GameService } from '../services/game.service';
-import {NgxChessBoardService} from 'ngx-chess-board';
+import { NgxChessBoardService } from 'ngx-chess-board';
+import { HttpErrorResponse } from '@angular/common/http';
 
+interface bot_move {
+    player_name: string;
+    move_gen: string;
+    depth: number;
+    id: number;
+}
 @Component({
     selector: 'app-game',
     templateUrl: './game.component.html',
@@ -13,26 +20,93 @@ export class GameComponent implements OnInit {
 
     @Output() change = new EventEmitter();
 
-    client_id: string | null = null;
+    client_id: string = '';
     player_move = true;
+    game_over = false;
     constructor(private readonly gameService: GameService, private ngxChessBoardService: NgxChessBoardService) { }
 
-    ngOnInit(): void {
+    async ngOnInit(): Promise<void> {
+        await this.start_new_game();
     }
+    bot_move_list: bot_move[] = [];
 
-    async start_new_game(){
+    async start_new_game() {
         this.board?.reset();
-        await this.gameService.init().toPromise().then(res=>{
-            if(res?.status){
+        this.game_over = false;
+        await this.gameService.init().toPromise().then(res => {
+            if (res?.status) {
                 this.board?.setFEN(res.data.fen);
                 this.client_id = res.data.client_id;
             }
+        }).catch((error: HttpErrorResponse) => {
+
         })
 
     }
 
-    logmove(data:any){
-        console.log(data.move)
+    async move(data: any) {
+        const move = data.move;
+        await this.make_move(move);
     }
+    async select_bot_move(id: number) {
+        if (id < this.bot_move_list.length && id >= 0) {
+            const move = this.bot_move_list[id].move_gen;
+            await this.make_move(move);    
+        }
+    }
+    async make_move(move: string) {
+        if (this.player_move) {
+            await this.gameService.player_move(this.client_id, move).toPromise()
+                .then(async res => {
+                    if (res?.status) {
+                        this.game_over = res.data.game_over;
+                        if(!this.game_over){
+                            await this.fetchBotMoves();
+                        }
+                    }
+                }).catch((error: HttpErrorResponse) => {
+
+                })
+        }
+        else {
+            await this.gameService.bot_move(this.client_id, move).toPromise()
+                .then(res => {
+                    if (res?.status) {
+                        this.game_over = res.data.game_over;
+                    }
+                }).catch((error: HttpErrorResponse) => {
+
+                })
+        }
+        this.player_move = !this.player_move;
+
+
+    }
+
+    async fetchBotMoves() {
+        await this.gameService.fetch_bot_moves(this.client_id).toPromise()
+            .then(res => {
+                this.bot_move_list = [];
+                if (res?.status) {
+                    let i = 0;
+                    this.bot_move_list.push({
+                        ...res.data[i],
+                        id: i++
+                    });
+                    this.bot_move_list.push({
+                        ...res.data[i],
+                        id: i++
+                    })
+                    this.bot_move_list.push({
+                        ...res.data[i],
+                        id: i++
+                    })
+                }
+            }).catch((error: HttpErrorResponse) => {
+
+            })
+    }
+
+  
 
 }
--- src/app/services/game.service.ts
+++ src/app/services/game.service.ts
@@ -7,9 +7,9 @@ import { ResponseModel } from "../models/util.model";
 @Injectable({
     providedIn: 'root'
 })
-export class GameService{
-  
-    constructor(private readonly http: HttpClient) {}
+export class GameService {
+
+    constructor(private readonly http: HttpClient) { }
 
     private handleError(error: HttpErrorResponse) {
         return throwError(error);
@@ -18,6 +18,21 @@ export class GameService{
     init(): Observable<ResponseModel> {
         return this.http
             .get<ResponseModel>(API_URL + 'v1/game/new')
+            .pipe(catchError(this.handleError));
+    }
+
+    player_move(client_id: string, move: string): Observable<ResponseModel> {
+        return this.http.put<ResponseModel>(API_URL + "/v1/game/player/move", { client_id, move })
+            .pipe(catchError(this.handleError));
+    }
+
+    fetch_bot_moves(client_id: string): Observable<ResponseModel> {
+        return this.http.put<ResponseModel>(API_URL + "/v1/game/bot/moves/fetch", { client_id })
+            .pipe(catchError(this.handleError));
+    }
+
+    bot_move(client_id: string, move: string): Observable<ResponseModel> {
+        return this.http.put<ResponseModel>(API_URL + "/v1/game/bot/move", { client_id, move })
             .pipe(catchError(this.handleError));
     }
 }
